4b313e871ded3a2a17082e23fa6c8045c596a986
Husni <zulhusni.rahman@gmail.com>
2017-01-09 17:30:21 +0100

    Solving casting error in ../test/makethreads.c

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -2,7 +2,7 @@
 
 void print(void* ch)
 {
-    char c = (char)ch;
+    char c = (char)((int)ch);
     int i;
     for (i = 0; i < 5; i++) {
         PutChar(c+i);
@@ -14,7 +14,12 @@ int main()
     //do_UserThreadCreate(int f, int arg);
     void* c = (void*)'a';
     int n = UserThreadCreate(print, c);
+    PutChar((char)n);
     //print('a');
+    char q = '0';
+    while(q != 'q')
+    	GetChar(q);
+
     Halt();
     return 0;
 }
