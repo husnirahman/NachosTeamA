af6d73ca7af3b5affd33c925f0f0eb5206f13249
Husni <zulhusni.rahman@gmail.com>
2017-01-11 13:37:43 +0100

    int i declaration error resolved in ../test/makethreads.c

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -25,7 +25,8 @@ int main()
     //Puts("Enter Number of Threads below 5: ");
     int n = 5;
     int Thread_id[2*n +1];
-    for(int i = 0; i<2*n; i+=2){
+    int i;
+    for(i = 0; i<2*n; i+=2){
     	Thread_id[i] = UserThreadCreate(print, (void*)&c);
     	//PutChar(' ');
     	Thread_id[i+1] = UserThreadCreate(print, (void*)&c);
@@ -34,7 +35,7 @@ int main()
     
     Thread_id[10] = UserThreadCreate(print2, (void*)&c);
     //PutInt(Thread_id[10]);
-    for(int i = 0 ; i<2*n ; i++)
+    for(i = 0 ; i<2*n ; i++)
     	UserThreadJoin(Thread_id[i]);
     Halt();
     return 0;
